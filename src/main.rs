875fbc4a3b7669dfbda5df8e644e1c35d6a12e34
Azhar-H1 <azhar.adamh37@gmail.com>
2025-04-23 13:45:06 +1000

    first testing iteration of echo

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 use std::io;
 
 fn main() {
-    println!("Stringback Test!");
+    println!("Stringback Test!\nEnter A String");
 
+    let mut input = String::new();
 
-}
+    io::stdin()
+    .read_line(&mut input)
+    .expect("Error Reading Input");
+
+    println!("Here's your String!\n--> {} <--", input);
+        // 1235467890123456790132456789013245678901234567890
+        /*  can't figure out why there's a hidden \n
+         on the second indenting arrow - not necessary
+            for the time being*/
+    }
