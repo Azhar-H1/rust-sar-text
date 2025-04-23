5010ec1eb6c2ab5e49a779bd6458162185774a13
Azhar-H1 <azhar.adamh37@gmail.com>
2025-04-23 15:48:26 +1000

    stashing current start of building new string attempt - might need to try just char instead of char indicies

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,13 @@ fn main() {
     .read_line(&mut input)
     .expect("Error Reading Input");
 
-    let mut chars: Vec<char> = input.chars().collect();
+    //let chars: Vec<char> = input.chars().collect();
 
-    for n  in 0..(chars.len()-1) {
-        println!("==> Current index {}", n);
-    //for n in chars.iter() {
-        if n % 2 == 0{
-            println!("==> Got Even Index @{} with {}", n, chars[n]);
-            if chars[n] as u32 >= 41 && chars[n] as u32 <= 90{
-                let prints: String =chars.clone().into_iter().collect();
-                println!("==> Attempting to modify: {}", prints )  ;
-                 chars[n].to_ascii_uppercase();
-            }
-        }
-    }
+    let s: String = input
+        .char_indices()
+        .map(f)
 
-    let s: String = chars.into_iter().collect();
-    println!("Here's your String!\n------\n{}\n------", s);
+
+    //println!("Here's your String!\n------\n{}\n------", );
         
     }
