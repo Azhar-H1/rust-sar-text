2e0cb00ce8f4874de522715056e91bbd7b162215
Azhar-H1 <azhar.adamh37@gmail.com>
2025-04-23 15:16:58 +1000

    old style not working, still relying on mutations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, iter};
 
 fn main() {
     println!("Stringback Test!\nEnter A String");
@@ -9,9 +9,22 @@ fn main() {
     .read_line(&mut input)
     .expect("Error Reading Input");
 
-    println!("Here's your String!\n--> {} <--", input);
-        // 1235467890123456790132456789013245678901234567890
-        /*  can't figure out why there's a hidden \n
-         on the second indenting arrow - not necessary
-            for the time being*/
+    let mut chars: Vec<char> = input.chars().collect();
+
+    for n  in 0..(chars.len()-1) {
+        println!("==> Current index {}", n);
+    //for n in chars.iter() {
+        if n % 2 == 0{
+            println!("==> Got Even Index @{} with {}", n, chars[n]);
+            if chars[n] as u32 >= 41 && chars[n] as u32 <= 90{
+                let prints: String =chars.clone().into_iter().collect();
+                println!("==> Attempting to modify: {}", prints )  ;
+                 chars[n].to_ascii_uppercase();
+            }
+        }
+    }
+
+    let s: String = chars.into_iter().collect();
+    println!("Here's your String!\n------\n{}\n------", s);
+        
     }
